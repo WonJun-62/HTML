31ced092c94a80351897add62cbb40c28a4f8316
WonJun-62 <132991365+WonJun-62@users.noreply.github.com>
2024-07-03 18:26:49 +0900

    practice 7/3

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
 
-  let posts = { color : 'blue', fontSize : '30px' };
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '강남 피씨방']);
+
+  let posts = '강남 고기 맛집'
 
   return (
     <div className="App">
+
       <div className="black-nav">
-        <div style={ posts }>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <img src={ logo }/>
-      <h4>  posts  </h4>
+
+      <div className="list">
+        <h3> { posts } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> { 글제목[0] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
