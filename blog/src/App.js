eb297344822297b0a932ebe248ca8fd07cbb527e
WonJun-62 <132991365+WonJun-62@users.noreply.github.com>
2024-07-06 23:20:28 +0900

    practice 7/6

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,5 @@
+/*eslint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -6,8 +8,23 @@ import { useState } from 'react';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '강남 피씨방']);
-
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집'
+
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+  }
+
+  function 순서바꾸기(){
+    var newArray = [...글제목];
+    var test = newArray[0];
+    newArray[0] = newArray[1];
+    newArray[1] = newArray[2];
+    newArray[2] = test;
+    글제목변경(newArray);
+  }
 
   return (
     <div className="App">
@@ -15,15 +32,10 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-
+      <button onClick={ 제목바꾸기 }>제목 버튼</button>
+      <button onClick={ 순서바꾸기 }>순서 버튼</button>
       <div className="list">
-        <h3> { posts } </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-
-      <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
